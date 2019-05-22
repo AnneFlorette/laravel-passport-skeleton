ea3340e0f9056323f5ae80d4a15c912eda108ce4
Stifeux <stifeu@hotmail.fr>
2019-05-22 14:22:23 +0200

    modif migration rajout cascade

--- database/migrations/2019_05_22_092229_create_tickets_table.php
+++ database/migrations/2019_05_22_092229_create_tickets_table.php
@@ -16,13 +16,15 @@ class CreateTicketsTable extends Migration
         Schema::create('tickets', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('title');
-            $table->string('content');
+            $table->string('content')->nullable();
             $table->enum('priority', [1, 2, 3]);
             $table->enum('state', ['PENDING', 'WAITING', 'IN_PROGRESS', 'DONE']);
             $table->timestamp('first_assignation')->nullable();
             $table->timestamp('last_assignation')->nullable();
-            $table->integer('user_id');
-            $table->integer('user_id_assigned');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id_assigned')->nullable();
+            $table->foreign('user_id_assigned')->references('id')->on('users')->onDelete('cascade');
 
             $table->timestamps(); //created_at and updated_at
         });

8c6860892ae1dc7a982b0ad666d518e14c2a82e6
Cyrille Carpentier <cyrille.carpentier@imie.fr>
2019-05-22 14:19:10 +0200

    user controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Http\UserControllers;
+
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\database\factories\UserFactory;
+use App\Http\Middleware\Authenticate as auth;
+use App\User;
+use App\Ticket;
+use App\Comment;
+
+class UserController extends BaseController
+{
+    //use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function show($id)
+    {
+        $user = User::findorfail($id);
+        return response()->json([
+            'name' => $user->name,
+            'email' => $user->email,
+        ]);
+    }
+
+    public function index()
+    {
+        return response()->json([
+            'user' => User::all()
+        ]);
+    }
+
+    public function create(Request $request)
+    {
+        $user = new User();
+
+        if(($request->input('name')) && ($request->input('email')) && ($request->input('password')))
+        {
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            $user->password = Hash::make($request->input('password'));
+            $user->save();
+        }
+    }
+
+    public function update(Request $request)
+    {
+        $user = new User();
+
+        if(($request->input('name')) && ($request->input('email')) && ($request->input('password')))
+        {
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            $user->password = Hash::make($request->input('password'));
+            $user->save();
+        }
+    }
+
+    public function delete($id)
+    {
+        $user = User::findorfail($id);
+        $ticket = Ticket::where('user_id', $id);
+        $comment = Comment::where('user_id', $id);
+
+        $comment->delete();
+        $ticket->delete();
+        $user->delete();
+    }
+
+    public function emailVerified($id)
+    {
+        $user = new User();
+
+        $user->email_verified_at = date('Y-m-d H:i:s');
+    }
+}

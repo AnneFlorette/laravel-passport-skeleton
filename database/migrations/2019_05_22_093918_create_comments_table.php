b5eba5186549ca8f6710e2a043370518ad97c1b9
Stifeux <stifeu@hotmail.fr>
2019-05-24 09:20:12 +0200

    Commentaire migration

--- database/migrations/2019_05_22_093918_create_comments_table.php
+++ database/migrations/2019_05_22_093918_create_comments_table.php
@@ -14,13 +14,13 @@ class CreateCommentsTable extends Migration
     public function up()
     {
         Schema::create('comments', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
+            $table->engine = 'InnoDB'; //Force la bdd sous InnoDB
             $table->bigIncrements('id');
             $table->string('content');
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-            $table->unsignedBigInteger('ticket_id');
-            $table->foreign('ticket_id')->references('id')->on('tickets')->onDelete('cascade');
+            $table->unsignedBigInteger('user_id'); //Definis la colonne comme non assignée
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade'); //Rend la colonne foreign key sur la table user avec une suppression si le user est supprimé
+            $table->unsignedBigInteger('ticket_id'); //Definis la colonne comme non assignée
+            $table->foreign('ticket_id')->references('id')->on('tickets')->onDelete('cascade'); //Rend la colonne foreign key sur la table user avec une suppression si le ticket est supprimé
             $table->timestamps();
         });
     }
